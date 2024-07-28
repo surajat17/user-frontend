b2942cafc3ed34047a4214e385310fdc139c1a5c
suraj kumar <suraj.kumar@knorex.com>
2024-07-29 00:43:26 +0530

    chane

--- src/app/version/page.tsx
+++ src/app/version/page.tsx
@@ -1,14 +1,19 @@
-// pages/version.js
+// app/version/page.tsx
 'use client'
 import { useEffect, useState } from 'react';
 
+interface VersionInfo {
+  commit: string;
+  date: string;
+}
+
 const Version = () => {
-  const [versionInfo, setVersionInfo] = useState(null);
+  const [versionInfo, setVersionInfo] = useState<VersionInfo | null>(null);
 
   useEffect(() => {
     fetch('/version.json')
       .then((response) => response.json())
-      .then((data) => {
+      .then((data: VersionInfo) => {
         setVersionInfo(data);
       });
   }, []);
@@ -21,7 +26,6 @@ const Version = () => {
     <div>
       <h1>Version Info</h1>
       <p>{`${versionInfo.date}-${versionInfo.commit}`}</p>
-      ok
     </div>
   );
 };
